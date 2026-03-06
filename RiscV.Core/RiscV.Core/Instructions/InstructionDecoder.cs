a492be2c6953e2ebbe4ce9b72d97fc1660ac9a1d
Andrei Ciobanu <andrei.ciobanu4@student.tuiasi.ro>
2026-03-06 13:12:43 +0200

    Initial arhitecture

--- RiscV.Core/RiscV.Core/Instructions/InstructionDecoder.cs
+++ RiscV.Core/RiscV.Core/Instructions/InstructionDecoder.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RiscV.Core
+namespace RiscV.Core.Instructions
 {
-    public class Class1
+    internal class InstructionDecoder
     {
     }
 }
