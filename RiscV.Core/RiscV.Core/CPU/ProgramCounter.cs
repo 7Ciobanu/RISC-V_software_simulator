ffb04e4907d7d99c9af4f81f0ead2b4391f3bceb
Andrei Ciobanu <andrei.ciobanu4@student.tuiasi.ro>
2026-03-09 13:04:14 +0200

    ALU and Program Counter

--- RiscV.Core/RiscV.Core/CPU/ProgramCounter.cs
+++ RiscV.Core/RiscV.Core/CPU/ProgramCounter.cs
@@ -8,5 +8,26 @@ namespace RiscV.Core.CPU
 {
     internal class ProgramCounter
     {
+        private uint pc;
+
+        public uint Get() 
+        {
+            return pc; 
+        }
+
+        public void Set(uint value) 
+        { 
+            pc = value; 
+        }
+
+        public void Increment(uint step = 4)
+        {
+            pc+= step;
+        }
+
+        public void Reset()
+        {
+            pc = 0;
+        }
     }
 }
--- RiscV.Core/RiscV.Core/Hardware/ALU.cs
+++ RiscV.Core/RiscV.Core/Hardware/ALU.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace RiscV.Core.Hardware
 {
+    enum ALUOperation
+    {
+        ADD,// Addition
+        SUB,// Subtraction
+        AND,// AND
+        OR,// OR
+        XOR,// XOR
+        SLL,// Shift left logical
+        SRL,// Shift right logical
+        SRA,// Shift right arithmetic
+        SLT,// Set less than signed
+        SLTU// Set less than unsigned
+    }
     internal class ALU
     {
+        public uint Execute(ALUOperation operation, uint operand1, uint operand2)
+        {
+            switch (operation)
+            {
+                case ALUOperation.ADD:
+                    return operand1 + operand2;
+
+                case ALUOperation.SUB:
+                    return operand1 - operand2;
+
+                case ALUOperation.AND:
+                    return operand1 & operand2;
+
+                case ALUOperation.OR:
+                    return operand1 | operand2;
+
+                case ALUOperation.XOR:
+                    return operand1 ^ operand2;
+
+                case ALUOperation.SLL: 
+                    return operand1 << (int)(operand2 & 0x1F);
+
+                case ALUOperation.SRL: 
+                    return operand1 >> (int)(operand2 & 0x1F);
+
+                case ALUOperation.SRA:
+                    return (uint)((int)operand1 >> (int)(operand2 & 0x1F));
+
+                case ALUOperation.SLT:
+                    return ((int)operand1 < (int)operand2) ? 1u : 0u;
+
+                case ALUOperation.SLTU:
+                    return (operand1 < operand2) ? 1u : 0u;
+
+                default:
+                    throw new InvalidOperationException("operation not supported");
+            }
+        }
     }
 }
