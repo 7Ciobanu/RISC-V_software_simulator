d6f9a4b2ec60824e50de2a0eed66f013277fcc5a
Andrei Ciobanu <andrei.ciobanu4@student.tuiasi.ro>
2026-04-21 14:41:00 +0300

    Small refactor

--- RiscV.Interface/Form1.cs
+++ RiscV.Interface/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using RiscV.Core.CPU;
 namespace RiscV.Interface
 {
     public partial class Form1 : Form
